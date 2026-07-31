092c9d154830a214eff46fc0d743295072139973
Luciano <lcastro@rioplatense.com>
2026-07-31 17:04:30 -0300

    Agregamos Postgis y ERD de tarifario

--- backend/transportista/models/__init__.py
+++ backend/transportista/models/__init__.py
@@ -1,3 +1,4 @@
 from .transportista_models import Transportista
+from .tarifario_models import Tarifario, TarifaFlete, ConceptoAdicional, TarifaConceptoAdicional
 
-__all__ = ["Transportista"]
+__all__ = ["Transportista", "Tarifario", "TarifaFlete", "ConceptoAdicional", "TarifaConceptoAdicional"]
